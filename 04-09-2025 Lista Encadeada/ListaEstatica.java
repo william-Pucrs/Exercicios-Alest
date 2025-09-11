a0bbe7e984d065150e544543e4881040e9f16399
william.silva96 <william.silva96@edu.pucrs.br>
2025-09-11 10:01:11 -0300

    completado Lista encadead

--- 04-09-2025 Lista Encadeada/ListaEstatica.java
+++ 04-09-2025 Lista Encadeada/ListaEstatica.java
@@ -2,62 +2,119 @@
 public class ListaEstatica {
 
     private int [] arranjo;
-    // fimDaLista tem duplo Sentido
-    // 1. Aprimeira posição livre para inserção
-    // 2. O numero de elementos salvos
-    private int fimDaLista;
+    // fimDaLista tem dupla interpretação
+    //   1. A primeira posição livre para inserção na lista
+    //   2. O nro de elmentos salvos na minha lista
+    private int fimDaLista; 
+
+    public ListaEstatica() {
+        this.arranjo = new int[10];
+        this.fimDaLista=0;        
+    }
+
+    public ListaEstatica(int capacidadeInicial) {
+        this.arranjo = new int[capacidadeInicial];
+        this.fimDaLista=0;        
+    }
+
+    private void aumentaAEstrutura(){
+        if(this.fimDaLista==this.arranjo.length){
+            int [] novoArranjo = new int[arranjo.length];
+            for(int i=0; i<arranjo.length; i++)
+                novoArranjo[i]=arranjo[i];
+            arranjo=novoArranjo;
+        }
+    }
 
     public boolean add(int e){
-        if(this.fimDaLista == this.arranjo.length){
-            return false;
-        }else 
-
+        aumentaAEstrutura();
         this.arranjo[this.fimDaLista] = e;
-        this.fimDaLista ++;
+        this.fimDaLista++;
+        return true;
     }
     //public void add(int index, int element){}
     //public boolean addAll(int[] c) 
     //public boolean addAll(int index, int[] c) 
     public void clear(){
-        this.fimDaLista = 0;
+        this.fimDaLista=0;
     }
     //public boolean contains(Object o) 
     public int get(int index){
-
-        
+        validaPosicao(index);
         return arranjo[index];
     }
     //public int indexOf(int o) 
     public boolean isEmpty(){
-       return this.fimDaLista == 0;
-
+        return this.fimDaLista==0;
     }
     //public int lastIndexOf(int o) 
     public int remove(int index){
-         validaPosicao(index);
-        // sava valor a ser retornado
+        validaPosicao(index);
+
+        //salva valor a ser retornado
         int aux = arranjo[index];
-        
-        // ajusata lista, remove espaçoes va\ios
-        for(int i = index; i < this.fimDaLista-1; i++){
-            arranjo[i] = arranjo[i+1];
-        } this.fimDaLista --;
+
+        // ajusta lista 
+        //   remove espaços vazios
+
+        for(int i=index; i<this.fimDaLista-1; i++)
+            this.arranjo[i]=this.arranjo[i+1];
+        this.fimDaLista--;
+
+        // retorna valor salvo
         return aux;
+    }
+
+    private void validaPosicao(int index){
+        if((index >= fimDaLista)||(index<0))
+            throw new RuntimeException("A posição index:"+index+" é inválida");
     }
     //public boolean remove(int o) 
     //public boolean removeAll(int[] c) 
-
-    private void validaPosicao(int index){
-        if ((index >= fimDaLista) || (index < 0)){
-            throw new RuntimeException(" deu ruim");
-        }
-    }
     //public int set(int index, int element) 
     public int size(){
-
         return this.fimDaLista;
     }
     //public int[] subList(int fromIndex, int toIndex) 
 
-   
+    public String toString(){
+
+        if(this.fimDaLista==0)
+            return "A lista está vazia";
+        
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("0:[");
+        for(int idx=0; idx<fimDaLista-1; idx++)
+            sb.append(arranjo[idx]+",");
+        sb.append(arranjo[fimDaLista-1]);
+        sb.append("]:"+(fimDaLista-1)+":"+(arranjo.length-1)+"("+fimDaLista+")");
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        ListaEstatica le = new ListaEstatica();
+
+        System.out.println(le);
+
+        le.add(32);
+        System.out.println(le);
+
+        le.add(14);
+        System.out.println(le);
+
+        le.add(-1);
+        System.out.println(le);
+
+        le.add(98);
+        System.out.println(le);
+
+        System.out.println("O conteudo da posicao 2 é "+le.get(2));
+
+        le.remove(1);
+        System.out.println(le);
+
+    }
 }
